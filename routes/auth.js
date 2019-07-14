a9dd28ecaf6e715ab7fd5395d7864fbe199b844f
Logan Wood <lwood3499@gmail.com>
2019-07-14 12:24:57 -0500

    route validation with Joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+// VALIDATION
+const Joi = require("@hapi/joi");
+
+const validationSchema = {
+	name: Joi.string()
+		.min(3)
+		.required(),
+	email: Joi.string()
+		.min(6)
+		.required()
+		.email(),
+	password: Joi.string()
+		.min(6)
+		.required()
+};
+
 // route: /api/user/register
 router.post("/register", async (req, res) => {
+	// Validate data before creating user
+	const { error } = Joi.validate(req.body, validationSchema);
+	if (error) return res.status(400).send(`Error Details: ${error.details[0].message}`);
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
