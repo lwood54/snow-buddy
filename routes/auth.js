8ac4dc654a25c1dd79cafbd28092fa972bff8cc3
Logan Wood <lwood3499@gmail.com>
2019-07-14 12:38:10 -0500

    separate validation into own file

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,14 @@
 const router = require("express").Router();
 const User = require("../model/User");
-
-// VALIDATION
-const Joi = require("@hapi/joi");
-
-const validationSchema = {
-	name: Joi.string()
-		.min(3)
-		.required(),
-	email: Joi.string()
-		.min(6)
-		.required()
-		.email(),
-	password: Joi.string()
-		.min(6)
-		.required()
-};
+const { registrationValidation } = require("../validation");
 
 // route: /api/user/register
 router.post("/register", async (req, res) => {
 	// Validate data before creating user
-	const { error } = Joi.validate(req.body, validationSchema);
+	const { error } = registrationValidation(req.body);
 	if (error) return res.status(400).send(`Error Details: ${error.details[0].message}`);
+
+	// define user if route info validated
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
---
+++ validation.js
@@ -0,0 +1,35 @@
+// VALIDATION
+const Joi = require("@hapi/joi");
+
+// User Registration Validation
+const registrationValidation = data => {
+	const validationSchema = {
+		name: Joi.string()
+			.min(3)
+			.required(),
+		email: Joi.string()
+			.min(6)
+			.required()
+			.email(),
+		password: Joi.string()
+			.min(6)
+			.required()
+	};
+	return Joi.validate(data, validationSchema);
+};
+
+const loginValidation = data => {
+	const validationSchema = {
+		email: Joi.string()
+			.min(6)
+			.required()
+			.email(),
+		password: Joi.string()
+			.min(6)
+			.required()
+	};
+	return Joi.validate(data, validationSchema);
+};
+
+module.exports.registrationValidation = registrationValidation;
+module.exports.loginValidation = loginValidation;
