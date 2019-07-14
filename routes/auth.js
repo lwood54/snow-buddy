8ce6ed6f4f6e71bd853beeedb456a01537509a45
Logan Wood <lwood3499@gmail.com>
2019-07-14 13:41:04 -0500

    encrypt password and validate user login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
+const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 const User = require("../model/User");
-const { registrationValidation } = require("../validation");
+const { registrationValidation, loginValidation } = require("../validation");
 
 // route: /api/user/register
 router.post("/register", async (req, res) => {
@@ -12,21 +13,39 @@ router.post("/register", async (req, res) => {
 	const emailExists = await User.findOne({ email: req.body.email });
 	if (emailExists) return res.status(400).send("User (email) already exists...");
 
+	// Hash the password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
 	// Create new user if route info validated
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password
+		password: hashedPassword
 	});
 	try {
 		const savedUser = await user.save();
-		res.send(savedUser);
+		res.send({ user: savedUser._id });
 	} catch (error) {
 		res.status(400).send(err);
 	}
 });
 
 // route: /api/user/login
-router.post("/login");
+router.post("/login", async (req, res) => {
+	// Validate data before creating user
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(`Error Details: ${error.details[0].message}`);
+
+	// Check if user is already in DB
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send("User not found...email or password may be wrong.");
+
+	// Check for correct PASSWORD
+	const validPassword = await bcrypt.compare(req.body.password, user.password);
+	if (!validPassword) return res.status(400).send("Password is invalid...");
+
+	res.send("Logged in!");
+});
 
 module.exports = router;
