30b91b73d574dee2479a20b13ebfb970666baf03
Logan Wood <lwood3499@gmail.com>
2019-08-10 15:25:16 -0500

    fin | landing bg slope

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,6 @@ import NavBar from "./navigation/navBar";
 import AppRouter from "./appRouter";
 import cls from "./styles/app.module.scss";
 
-import bgSlopeSVG from "./images/svgs/slope-cropped.svg";
-import bgSlopeSVG3 from "./images/svgs/slope-3.svg";
-import bgSlopeSVG4 from "./images/svgs/slope4-white.svg";
-
 export const LoggedInStatus = createContext(false);
 export const CurrentUserContext = createContext({});
 
@@ -15,9 +11,6 @@ function App() {
 	const [currentUser, setCurrentUser] = useState({});
 	return (
 		<div className={cls.mainContainer}>
-			<svg>
-				<path d="M0,0 Q10,10 Z" />
-			</svg>
 			<LoggedInStatus.Provider value={[isLoggedIn, setIsLoggedIn]}>
 				<CurrentUserContext.Provider value={[currentUser, setCurrentUser]}>
 					<NavBar />
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import cls from "../styles/home.module.scss";
+import bgSlopeSVGtall from "../images/svgs/slope-tall.svg";
+
 const home = () => {
 	return (
-		<div>
+		<div className={cls.mainLandingContainer}>
+			<img className={cls.bgImage} src={bgSlopeSVGtall} alt="bg-slope" />
+			<div className={cls.slopeFiller} />
 			<h1>Home</h1>
 		</div>
 	);
