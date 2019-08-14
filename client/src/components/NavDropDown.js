25bff934773d5a299f0a28a0644bd29ad3299acd
Logan Wood <lwood3499@gmail.com>
2019-08-13 23:52:19 -0500

    wip | menu animations

--- client/src/components/NavDropDown.js
+++ client/src/components/NavDropDown.js
@@ -8,22 +8,24 @@ import { LoggedInStatus } from "../App";
 const NavDropDown = ({ isOpen }) => {
 	const [isLoggedIn] = useContext(LoggedInStatus);
 	return (
-		<div className={[cls.navLinksContainer, isOpen ? cls.menuSlideIn : cls.menuSlideOut].join(" ")}>
-			<Link to="/" className={cls.linkStyle}>
-				<div className={cls.inner}>Home</div>
-			</Link>
-			<Link to="/resorts/" className={cls.linkStyle}>
-				Resorts
-			</Link>
-			<Link to="/equipment-tips/" className={cls.linkStyle}>
-				Equipment Tips
-			</Link>
-			<Link to="/travel-tips/" className={cls.linkStyle}>
-				Travel Tips
-			</Link>
-			<Link to="/userProfile/" className={cls.linkStyle}>
-				{isLoggedIn ? "User Profile" : "Login/Register"}
-			</Link>
+		<div className={cls.navSliderContainer}>
+			<div className={[cls.navLinksContainer, isOpen ? cls.menuSlideIn : cls.menuSlideOut].join(" ")}>
+				<Link to="/" className={cls.linkStyle}>
+					<div className={cls.inner}>Home</div>
+				</Link>
+				<Link to="/resorts/" className={cls.linkStyle}>
+					Resorts
+				</Link>
+				<Link to="/equipment-tips/" className={cls.linkStyle}>
+					Equipment Tips
+				</Link>
+				<Link to="/travel-tips/" className={cls.linkStyle}>
+					Travel Tips
+				</Link>
+				<Link to="/userProfile/" className={cls.linkStyle}>
+					{isLoggedIn ? "User Profile" : "Login/Register"}
+				</Link>
+			</div>
 		</div>
 	);
 };
