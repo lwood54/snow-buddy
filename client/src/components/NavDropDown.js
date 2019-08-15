269c5c2fd5b79ffcf3e99eb169a3c6f81f49773d
Logan Wood <lwood3499@gmail.com>
2019-08-14 23:53:31 -0500

    wip | hiding initial animation on page load

--- client/src/components/NavDropDown.js
+++ client/src/components/NavDropDown.js
@@ -1,15 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import cls from "../styles/navDropDown.module.scss";
+import "../styles/navDropDown.module.scss";
 
 import { LoggedInStatus } from "../App";
 
 const NavDropDown = ({ isOpen }) => {
 	const [isLoggedIn] = useContext(LoggedInStatus);
+
+	useEffect(() => {
+		setTimeout(() => {
+			let hideContainer = document.getElementsByClassName(cls.hide);
+			console.log(hideContainer);
+			hideContainer[0].classList.remove(cls.hide);
+			console.log(hideContainer);
+		}, 300);
+		// return clearTimeout(removeHide);
+	}, []);
 	return (
-		<div className={cls.navSliderContainer}>
-			<div className={[cls.navLinksContainer, isOpen ? cls.menuSlideIn : cls.menuSlideOut].join(" ")}>
+		<div className={cls.hide} id="hideContainer">
+			<div className={[cls.navLinksContainer, isOpen ? cls.menuSlideIn : cls.menuSlideOut, "hide"].join(" ")}>
 				<Link to="/" className={cls.linkStyle}>
 					<div className={cls.inner}>Home</div>
 				</Link>
