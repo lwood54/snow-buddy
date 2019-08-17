5b3e69e984069a393e60f2823fbc7fd9d043524b
Logan Wood <lwood3499@gmail.com>
2019-08-16 23:10:38 -0500

    bg gradient userProfile

--- client/src/pages/userProfile.js
+++ client/src/pages/userProfile.js
@@ -4,12 +4,13 @@ import UserInfo from "../components/UserInfo";
 import UserLogin from "../components/UserLogin";
 import UserLogout from "../components/UserLogout";
 import UserRegistration from "../components/UserRegistration";
+import cls from "../styles/userProfile.module.scss";
 
 const UserProfile = () => {
 	const [isLoggedIn] = useContext(LoggedInStatus);
 	// return <div>{!isLoggedin ? <UserLogin /> : <UserInfo />}</div>;
 	return (
-		<div>
+		<div className={cls.background}>
 			<UserRegistration />
 			{isLoggedIn ? <UserLogout /> : <UserLogin />}
 			<UserInfo />
